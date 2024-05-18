23948fb94abd4c9755f3d2fa8fd841a5689a43a6
Nouran-Aly <109301713+Nouran-Aly@users.noreply.github.com>
2024-05-18 09:32:06 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+var quoteArray = [
+    "“The greatest glory in living lies not in never falling, but in rising every time we fall.”",
+    "“The way to get started is to quit talking and begin doing.”",
+    "“Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.”",
+    "“The future belongs to those who believe in the beauty of their dreams.”",
+    "“If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.”",
+    "“If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.”",
+    "“You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one.”",
+    "“You must be the change you wish to see in the world.”",
+    "“Spread love everywhere you go. Let no one ever come to you without leaving happier.”",
+];
+
+var authorArray = [
+    "-- Nelson Mandela",
+    "-- Walt Disney ",
+    "-- Steve Jobs",
+    "-- Eleanor Roosevelt",
+    "-- Oprah Winfrey",
+    "-- James Cameron",
+    "-- John Lennon",
+    "-- Mahatma Gandhi",
+    "-- Mother Teresa"
+];
+
+function newQuote() {
+    var randomNumber = Math.floor(Math.random() * (quoteArray.length));
+    document.getElementById("displayQuote").innerHTML = quoteArray[randomNumber];
+    document.getElementById("displayAuthor").innerHTML = authorArray[randomNumber];
+};
